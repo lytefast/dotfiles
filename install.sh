53230e18b95da5b92412580326958c718279df80
Sam S <lytefast@gmail.com>
2022-08-19 18:30:42 -0700

    rm mplayer +  smarter install.sh

--- install.sh
+++ install.sh
@@ -4,27 +4,35 @@ set -e
 dotfiles_dir=~/code/dotfiles
 
 set_and_backup_file() {
-  local filename=$1
+  local directory=$1
+  local filename=$2
+
+  if [[ ! -z "$directory" ]]; then
+    mkdir -p ~/$directory
+    filename=$directory/$filename
+  fi
+
   if test -f ~/$filename; then
     mv ~/$filename ~/$filename.bk
-    echo "$filename.bk created"
+    echo "~/$filename.bk created"
   fi
   ln -sf $dotfiles_dir/$filename ~/$filename
+
+  echo "~/$filename symlinked"
 }
 
-echo '== Symlinking file in' $dotfiles_dir ' =='
+echo '== Symlinking files in' $dotfiles_dir ' =='
 
 # Symlink config files
-set_and_backup_file .dev_env_rc
-set_and_backup_file .samrc
-set_and_backup_file .gitconfig
-set_and_backup_file .mplayer
-set_and_backup_file .vimrc
+set_and_backup_file "" .dev_env_rc
+set_and_backup_file "" .samrc
+set_and_backup_file "" .gitconfig
+set_and_backup_file "" .vimrc
 
-set_and_backup_file .config/Code/User/keybindings.json
-set_and_backup_file .config/Code/User/settings.json
+set_and_backup_file .config/Code/User keybindings.json
+set_and_backup_file .config/Code/User settings.json
 
-set_and_backup_file .config/mpv/mpv.conf
+set_and_backup_file .config/mpv mpv.conf
 
 echo '== Setup zsh =='
 echo 'see https://github.com/lytefast/prezto'
